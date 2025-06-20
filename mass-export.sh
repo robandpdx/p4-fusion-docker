4c6b52b24ff12bdcb711c05f413ccb4ada443abe
Rob Anderson <robandpdx@github.com>
2025-06-20 10:21:30 +0100

    add mass-export.sh script

---
+++ mass-export.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This script will read the config.json and convert the p4 depots and branches to git
+
+jq -c '.[]' config.json | while read -r depot; do
+    DEPOT_PATH=$(echo "$depot" | jq -r '.depotPath')
+    echo "Processing depot path: $DEPOT_PATH"
+
+    # DEPOT_PATH is "//project/stuff/gl-exporter/..." or "//gl-exporter/...". Parse out the depot name before "/..."
+    # Extract everything after the last "//" up to the last "/..."
+    DEPOT_NAME=$(echo "$DEPOT_PATH" | sed -E 's|^//(.*/)*([^/]+)/\.\.\.$|\2|')
+    echo "Depot name: $DEPOT_NAME"
+
+    BRANCH_INCLUDE=""
+
+    # Iterate over each branch in the branchConfig array for this depot
+    echo "$depot" | jq -c '.branchConfig[]' | while read -r branch_config; do
+        branch_name=$(echo "$branch_config" | jq -r '.branch')
+        echo "Processing branch: $branch_name for depot: $DEPOT_PATH"
+
+        # Here we update the BRANCH_INCLUDE variable to include the branch name
+        if [ -z "$BRANCH_INCLUDE" ]; then
+            BRANCH_INCLUDE="--branch $branch_name"
+        else
+            BRANCH_INCLUDE="$BRANCH_INCLUDE --branch $branch_name"
+        fi
+    done
+
+    # Run the p4-fusion command inside the Docker container
+    docker run -u $(id -u):$(id -g) \
+    -e P4PORT=$P4PORT \
+    -e P4USER=$P4USER \
+    -e P4CLIENT=$P4CLIENT \
+    -v $(pwd)/bare-clones:/p4-fusion/bare-clones \
+    --mount type=bind,source="$P4TICKETS",target=/home/ubuntu/.p4tickets,readonly \
+    --mount type=bind,source="$P4TRUST",target=/home/ubuntu/.p4trust,readonly \
+    --mount type=bind,source="$P4ENVIRO",target=/home/ubuntu/.p4enviro,readonly \
+    ghcr.io/robandpdx/p4-fusion:latest \
+    /p4-fusion/build/p4-fusion/p4-fusion \
+        --path $DEPOT_PATH \
+        --user "$P4USER" \
+        --port "$P4PORT" \
+        --client "$P4CLIENT" \
+        --src bare-clones/$DEPOT_NAME.git \
+        --networkThreads 20 \
+        --printBatch 100 \
+        --lookAhead 1000 \
+        --retries 10 \
+        --refresh 100 \
+        $BRANCH_INCLUDE
+
+done
